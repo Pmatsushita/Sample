cf72d22b31a5ef2a5f99de82040eacd425102f7b
KengoHayashi <hayashi.kengo@jp.panasonic.com>
2022-01-25 22:48:33 +0900

    add printMyName function

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@ int add(int a, int b)
     return a+b;
 }
 
+void printMyName(char* name)
+{
+    printf("My mane is %s\n", name);
+}
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello World" << std::endl;
-
+    printMyName("Matsushita");
+    
     return 0;
 }
